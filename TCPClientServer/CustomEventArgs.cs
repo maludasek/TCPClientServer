87fca9806f295c756e18d56fc44585b4c37c002d
Tomasz Filipowicz <maludasek@gmail.com>
2017-12-05 11:47:59 +0100

    Biblioteka TCPClientServerLibrary: obsługa zdarzeń (server start&stop, clientConnected, textReceived&SendToAll) + podstawowe zmienne.

---
+++ TCPClientServer/CustomEventArgs.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClientServer
+{
+    // Argumenty zdarzenia OnRaiseServerStartEvent
+    public class ServerStartEventArgs : EventArgs
+    {
+        public string ServerIP { get; set; }
+        public int Port { get; set; }
+        public ServerStartEventArgs(string serverIP, int port)
+        {
+            ServerIP = serverIP;
+            Port = port;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseServerConnectedEvent
+    public class ServerConnectedEventArgs : EventArgs
+    {
+        public string ServerIp { get; set; }
+        public string ServerPort { get; set; }
+        public ServerConnectedEventArgs(string serverIp, string serverPort)
+        {
+            ServerIp = serverIp;
+            ServerPort = serverPort;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseServerDisconnectedEvent
+    public class ServerDisconnectedEventArgs : EventArgs
+    {
+        public string ServerIp { get; set; }
+        public string ServerPort { get; set; }
+        public ServerDisconnectedEventArgs(string serverIp, string serverPort)
+        {
+            ServerIp = serverIp;
+            ServerPort = serverPort;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseClientDisconnectedEvent
+    public class ClientDisconnectedEventArgs : EventArgs
+    {
+        public string OldClient { get; set; }
+        public int ClientCount { get; set; }
+        public ClientDisconnectedEventArgs(string oldClient, int clientCount)
+        {
+            OldClient = oldClient;
+            ClientCount = clientCount;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseClientConnectedEvent
+    public class ClientConnectedEventArgs : EventArgs
+    {
+        public string NewClient { get; set; }
+        public int ClientCount { get; set; }
+        public ClientConnectedEventArgs(string newClient, int clientCount)
+        {
+            NewClient = newClient;
+            ClientCount = clientCount;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseTextReceivedEvent
+    public class TextReceivedEventArgs : EventArgs
+    {
+        public string ClientSender { get; set; }
+        public string TextReceived { get; set; }
+        public TextReceivedEventArgs(string clientSender, string textReceived)
+        {
+            ClientSender = clientSender;
+            TextReceived = textReceived;
+        }
+    }
+
+    // Argumenty zdarzenia OnRaiseTextSendToAllEvent
+    public class TextSendToAllEventArgs : EventArgs
+    {
+        public string TextSend { get; set; }
+        public TextSendToAllEventArgs(string textSend)
+        {
+            TextSend = textSend;
+        }
+    }
+
+}
---
+++ TCPClientServer/SocketServerAsync.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClientServer
+{
+    public class SocketServerAsync
+    {
+        // IP Serwera
+        IPAddress mIP;
+        // Port Serwera
+        int mPort;
+        // Listener Serwera
+        TcpListener mTcpListener;
+        // Lista klientów podłączonych do serwera
+        List<TcpClient> mClients;
+        // Zmienna Czy Serwer pracuje
+        public bool KeepRunning { get; set; }
+
+        // Wyjątki
+        // Zgłoszenie zdarzenia Startu Serwera
+        public EventHandler<ServerStartEventArgs> RaiseServerStartEvent;
+        // Zgłoszenie zdarzenia Odłączenia się klienta od Serwera
+        public EventHandler<ClientDisconnectedEventArgs> RaiseClientDisconnectedEvent;
+        // Zgłoszenie zdarzenia Podłączenia się klienta do Serwera
+        public EventHandler<ClientConnectedEventArgs> RaiseClientConnectedEvent;
+        // Zgłoszenie zdarzenia od Serwera Tekst odebrany 
+        public EventHandler<TextReceivedEventArgs> RaiseTextReceivedEvent;
+        // Zgłoszenie zdarzenia od Serwera Tekst wysłany do wszystkich klientów
+        public EventHandler<TextSendToAllEventArgs> RaiseTextSendToAllEvent;
+
+        // Konstruktor
+        public SocketServerAsync()
+        {
+            mClients = new List<TcpClient>();
+        }
+
+        // Obsługa zdarzenia Start Serwera
+        protected virtual void OnRaiseServerStartEvent(ServerStartEventArgs e)
+        {
+            // Wywołanie zdarzenia w tarym formacie?
+            //EventHandler<ServerStartEventArgs> handler = RaiseServerStartEvent;
+            //if (handler != null)
+            //{
+            //    handler(this, e);
+            //}
+
+            // Wywołanie zdarzenia
+            RaiseServerStartEvent?.Invoke(this, e);
+        }
+
+        // Obsługa zdarzenia Odłączenia się klienta od Serwera
+        protected virtual void OnRaiseClientDisconnectedEvent(ClientDisconnectedEventArgs e)
+        {
+            // Wywołanie zdarzenia
+            RaiseClientDisconnectedEvent?.Invoke(this, e);
+        }
+
+        // Obsługa zdarzenia Podłączenia się klienta do Serwera
+        protected virtual void OnRaiseClientConnectedEvent(ClientConnectedEventArgs e)
+        {
+            // Wywołanie zdarzenia
+            RaiseClientConnectedEvent?.Invoke(this, e);
+        }
+
+        // Obsługa zdarzenia od Serwera Tekst odebrany 
+        protected virtual void OnRaiseTextReceivedEvent(TextReceivedEventArgs e)
+        {
+            // Wywołanie zdarzenia
+            RaiseTextReceivedEvent?.Invoke(this, e);
+        }
+
+        // Obsługa zdarzenia od Serwera Tekst wysłany do wszystkich klientów
+        protected virtual void OnRaiseTextSendToAllEvent(TextSendToAllEventArgs e)
+        {
+            // Wywołanie zdarzenia
+            RaiseTextSendToAllEvent?.Invoke(this, e);
+        }
+    }
+}
